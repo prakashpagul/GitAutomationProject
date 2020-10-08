d69c92b121c3df5732e4afa32bceecac875623c9
shri <shri@DESKTOP-0JOUEUP>
2020-10-08 15:35:26 +0530

    method added

--- Online_Java_Selenium_6_July/src/javaPackage/Sum.java
+++ Online_Java_Selenium_6_July/src/javaPackage/Sum.java
@@ -5,8 +5,13 @@ public class Sum {
 		
 		int a=10;
 		int b =20;
-		
+		Sum s1 = new Sum ();
 		System.out.println(a+b);
+		s1.k();
+	}
+	
+	public void k() {
+		System.out.println("My Name is Shammi Jha");
 	}
 
 }
