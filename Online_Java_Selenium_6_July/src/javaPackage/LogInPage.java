ddd316956a698161d9ff303b769933a6d5a67837
shri <shri@DESKTOP-0JOUEUP>
2020-10-08 15:31:03 +0530

    2 Methods Are Creating in this Class

--- Online_Java_Selenium_6_July/src/javaPackage/LogInPage.java
+++ Online_Java_Selenium_6_July/src/javaPackage/LogInPage.java
@@ -3,8 +3,18 @@ package javaPackage;
 public class LogInPage {
 	public static void main(String[] args) {
 		
-		
+		LogInPage l1 = new LogInPage();
 		System.out.println("JAY BHAURAYA");
+		a();
+		
+		l1.b();
 	}
 
+	public static void a() {
+		System.out.println("My name is Rahul");
+	}
+	
+	public void b() {
+		System.out.println(1+2);
+	}
 }
